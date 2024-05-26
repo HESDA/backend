0d77ccccedd8a9441c4683cf39cd24e1273c13e1
HESDA <febrian.anakbrebes354@gmail.com>
2024-05-26 14:28:15 +0700

    store

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -2,19 +2,9 @@ const { Firestore } = require('@google-cloud/firestore');
 
 async function storeData(id, data) {
   const db = new Firestore();
+
   const predictCollection = db.collection('predictions');
   return predictCollection.doc(id).set(data);
 }
 
-async function getAllData() {
-  const db = new Firestore();
-  const predictCollection = db.collection('predictions');
-  const snapshot = await predictCollection.get();
-  const data = snapshot.docs.map(doc => ({
-    id: doc.id,
-    history: doc.data()
-  }));
-  return data;
-}
-
-module.exports = { storeData, getAllData };
+module.exports = storeData;
